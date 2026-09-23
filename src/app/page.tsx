0bc47d29ff7e2a32a25b865d14c82a9807e0b6d8
artufin <arturo.herreros@uc.cl>
2026-09-23 13:37:02 -0300

    fix: increase interval for PartyEffect to enhance video playback duration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default async function HomePage({ searchParams }: Props) {
       <PartyEffect
         video="/videos/esqueleto.mp4"
         sound="/audio/bad-to-the-bone.mp3"
-        intervalMs={10000}
+        intervalMs={600000}
       />
 
       <form action={salir}>
